c16f8637ade5459d80cc5a66bb814a81c35bfe55
Marcus Hansen <marcusEhansen@gmail.com>
2021-01-09 10:26:28 -0600

    adds login feature

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt');
 const SALT_ROUNDS = 6;
 
+
 const userSchema = new Schema({
     firstName: String,
     lastName: String,
@@ -14,14 +15,26 @@ const userSchema = new Schema({
     password: String
 }, { timestamps: true });
 
+userSchema.set('toJSON', {
+    transform: function(doc, ret) {
+        delete ret.password
+        return ret;
+    }
+});
+
 userSchema.pre('save', function(next) {
     const user = this;
     if(!user.isModified("password")) return next();
     bcrypt.hash(user.password, SALT_ROUNDS, function(err, hash) {
-        if(err) return next(err)
+        if(err) return next(err);
         user.password = hash;
         next();
     });
 });
+
+userSchema.methods.comparePassword = function(tryPassword, callback) {
+    bcrypt.compare(tryPassword, this.password, callback)
+}
+
 
 module.exports = mongoose.model('User', userSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,16 +1,8 @@
-// require modules
 const router = require('express').Router();
 const usersCtrl = require('../../controllers/users');
 
-// setup router object
 
+router.post('/signup', usersCtrl.signup);
+router.post('/login', usersCtrl.login);
 
-
-// require the controller
-
-
-// define a signup route
-router.post('signup', usersCtrl.signup);
-
-// export your router
 module.exports = router;
